92926f142a8e6a66f4a7f517798d69d8c7d2632b
Carrras <leonardocarradore@gmail.com>
2021-01-29 17:59:13 -0300

    Novo comando

--- bot.js
+++ bot.js
@@ -4,6 +4,8 @@ const Discord = require('discord.js')
 const client =  new Discord.Client()
 const BOT_PREFIX = '!'
 const SPURS_COMAND = 'jefbale'
+const JEFTE_COMMAND = 'jefte'
+
 
 client.on('ready', () => {
     console.log('Jefbale is ready to chapar o feliano')
@@ -12,5 +14,13 @@ client.on('message', msg =>{
     if (msg.content === `${BOT_PREFIX}${SPURS_COMAND}`){
         msg.reply('COME ON SPURS')
     }
+
+    if (msg.content === `${BOT_PREFIX}${JEFTE_COMMAND}`){
+        message.channel.send('', {
+            files: [
+                "https://i.ibb.co/Sv25NWq/unknown.png"
+            ]})
+    }
+
 })
 client.login(process.env.BOT_TOKEN)
